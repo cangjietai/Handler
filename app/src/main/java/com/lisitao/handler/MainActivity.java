0d02e045d2a043c1ca681e2d6bcb2ad69d0cdf9d
cangjietai <1296172553@qq.com>
2022-06-09 23:34:26 +0800

    传统方式在主线程中更新UI

--- app/src/main/java/com/lisitao/handler/MainActivity.java
+++ app/src/main/java/com/lisitao/handler/MainActivity.java
@@ -2,13 +2,40 @@ package com.lisitao.handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.text);
+        Button changeText = findViewById(R.id.change_text);
+        changeText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.change_text:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                      text.setText("Nice to meet you");
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
     }
 }
